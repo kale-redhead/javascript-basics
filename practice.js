b3521502f8c206b0cbc27456ab74d752b4f2e8a6
kaley forster <55896549+kale-redhead@users.noreply.github.com>
2021-04-07 16:38:23 -0400

    everything done but 14

--- practice.js
+++ practice.js
@@ -2,19 +2,19 @@
 
 //Create a variable called myName that is a string data type
 
-let myName = "Kaley";
+let myName = "Kaley"
 
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
-let myAge = 21;
+let myAge = 21
 
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
-let lovesCode = true;
+let lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 
@@ -101,15 +101,14 @@ function nameCheck(name){
 // The add function should return the two parameters added together
 
 function add(num1, num2){
-  let mathSum = num1 + num2;
   return num1 + num2;
 }
 
 //Now invoke add, passing in the numbers 3 and 4
 //storing the result in the variable mathSum.
 
-console.log(add(3,4));
 
+let mathSum = add(3, 4);
 
 
 //////////////////PROBLEM 12////////////////////
@@ -173,14 +172,11 @@ let pondScope = ["duck", "realDuck"];
 
 //Create a variable called age with your age assigned to you
 
-let age = 21;
+
 
 // FLASH FORWARD TO NEXT YEAR
 // reassign the value of age to be one greater than it was, because, we all get older
 
-let bdayAge = age + 1;
-console.log('this is age after birthday: ' + bdayAge)
+
 
 // Good news! We can live forever. Set your age to 999
-
-let newAge = 999;
